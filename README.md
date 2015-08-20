4ccdb2c96747129c7b5e924d6b5f672011bdd1d5
Artur Stępień <artur.stepien@bestproject.pl>
2015-08-20 17:36:55 +0200

    Added accurate calculation option. Few usability changes.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# WoT\WN8 1.04
+# WoT\WN8 1.1
 PHP class for processing World of Tanks WN8 stat value. This class require instance of Wargamin\API that can be downloaded from here https://github.com/artur-stepien/wargaming-papi
 
 ##Sample usage##
@@ -26,5 +26,10 @@ try {
 ```
 
 ##News##
+
+###1.1 - 2015-08-20###
+Added option to calculate accurate WN8. When this is enabled tanks missing in expected tank values will be removed from account summary. Notice that accurate calculation is from 25% to 35% slower because require stats for each missing tank. 
+Also loading expected tank values has been moved to seperate method to allow overriding.
+
 ###1.00 - 2015-08-19###
 Class published
--- wn8.php
+++ wn8.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     Wargaming.API
- * @version     1.00
+ * @version     1.1
  * @author      Artur Stępień (artur.stepien@bestproject.pl)
  * @copyright   Copyright (C) 2015 Artur Stępień, All rights reserved.
  * @license     GNU General Public License version 3 or later; see LICENSE.txt
@@ -24,11 +24,14 @@ class WN8 {
 	// Should application search for info about tanks missing in WN8 calculation
 	protected $search_missing_tanks = false;
 	
+	// Accurate calculation (removes OP and missing tanks from account summary data)
+	protected $accurate_calculation = false;
+	
 	// Account WN8
-	public $wn8;
+	protected $wn8;
 	
 	// Account ID
-	public $account_id;
+	protected $account_id;
 	
 	// Tanks missing in expected tank values that user drive (excluded from WN8 acounting)
 	public $missing_tanks;
@@ -42,29 +45,22 @@ class WN8 {
 	 * @param   \Wargaming\API   $api                            Wargaming\API instance version at least 1.04. It is used to retrieve data from Wargaming servers.
 	 * @param   string/integer   $search                         If $search is integer type application will assume it is account_id, if it is string application 
 	 *                                                           will search for account with nickname set to $search
+	 * @param   bool             $accurate_calculation           If $accurate_calculation is set to TRUE, application will remove OP tanks same as tanks missing in expected tank values 
+	 *                                                           from account summary. Warning: Accurate calculation is from 25% to 35% slower.
+	 * @param   bool             $missing_search                 If $missing_search is set to TRUE, application will also add informations about tanks missing in calculation (missing in expected tank values)
 	 * @param   array            $expected_tank_values_version   Array of expected tank values from wnefficiency.net (vBAddict.net)
-	 * @param   bool             $missing_search                 If $missing_search is set to TRUE, application will also add informations about tanks missing 
 	 *                                                           in WN8 calculation (those who are excluded from expected tank values due to being OP or lack of statistic data)
 	 * 
 	 * @throws \Exception
 	 */
-	public function __construct(\Wargaming\API $api, $search, $expected_tank_values_version = 21, $missing_search = false) {
+	public function __construct(\Wargaming\API $api, $search, $accurate_calculation = false, $missing_search = false, $expected_tank_values_version = 21 ) {
 		$this->api = $api;
+		$this->accurate_calculation = $accurate_calculation;
 		$this->search_missing_tanks = $missing_search;
 		
-		// Be sure we have expected tank values
-		if ( !file_exists(dirname(__FILE__).'/expected_tank_values_'.$expected_tank_values_version.'.json') ) {
-			file_put_contents(
-				dirname(__FILE__).'/expected_tank_values_'.$expected_tank_values_version.'.json', 
-				file_get_contents('http://www.wnefficiency.net/exp/expected_tank_values_'.$expected_tank_values_version.'.json')
-			);
-		}
 		
-		// Load expected tank values
-		$buff = json_decode( file_get_contents(dirname(__FILE__).'/expected_tank_values_21.json') )->data;
-		foreach ( $buff AS $tank ) {
-			$this->expected_tank_values[$tank->IDNum] = $tank;
-		}
+		// Moved loading of expected tank values to seperate method to allow method overriding
+		$this->loadExpectedTankValues($expected_tank_values_version);
 		
 		// If user provided nickname instead of account_id, get account_id
 		if( is_string($search) ) {
@@ -90,16 +86,32 @@ class WN8 {
 			$this->account_id = $search;
 			
 		}
+
+	}
+	
+	/**
+	 * Moved loading of expected tank values to seperate method to allow method overriding. 
+	 * You can use this method to load expected tank values from database or memmory cache.
+	 * 
+	 * @param   Integer   $version   Expected tank values version
+	 * @since   1.1
+	 */
+	protected function loadExpectedTankValues($version) {
 		
-		// Try to measure account WN8 stat
-		try {
+		// If we dont have expected tank values download them
+		if ( !file_exists(dirname(__FILE__).'/expected_tank_values_'.$version.'.json') ) {
+			file_put_contents(
+				dirname(__FILE__).'/expected_tank_values_'.$version.'.json', 
+				file_get_contents('http://www.wnefficiency.net/exp/expected_tank_values_'.$version.'.json')
+			);
+		}
+		
+		// Load expected tank values
+		$buff = json_decode( file_get_contents(dirname(__FILE__).'/expected_tank_values_'.$version.'.json') )->data;
+		foreach ( $buff AS $tank ) {
 			
-			$this->getWN8();
-			
-		// Catch any errors while counting WN8
-		} catch(\Exception $e) {
-			
-			throw new \Exception($e->getMessage(), $e->getCode(), $e->getPrevious());
+			// Load tanks values and index them by Tank ID
+			$this->expected_tank_values[$tank->IDNum] = $tank;
 			
 		}
 	}
@@ -117,36 +129,69 @@ class WN8 {
 			// Get account_id
 			$account_id = $this->account_id;
 			
-			// Get average values
-			$average = $this->api->get('wot/account/info',array(
+			// Get summary values
+			$summary = $this->api->get('wot/account/info',array(
 			   'fields'=>'statistics.all.battles,statistics.all.frags,statistics.all.damage_dealt,statistics.all.dropped_capture_points,statistics.all.spotted,statistics.all.wins',
 			   'account_id'=>$account_id
 			))->$account_id->statistics->all;
 			
-			// Get tanks info (battles count)
+			// Get tanks values
 			$tanks = $this->api->get('wot/account/tanks',array('fields'=>'tank_id,statistics.battles','account_id'=>$account_id))->$account_id;
+			
+			// If this account has no tanks data skip calculation and return 0
+			if( empty($tanks)) {
+				
+				$this->wn8 = 0;
+				return $this->wn8;
+				
+			}
 
 			// WN8 expected calculation
 			$expDAMAGE = $expFRAGS = $expSPOT = $expDEF = $expWIN = 0;
+			
+			// Tanks missing in expected tank values but existing in account
 			$missing = array();
 			
+			// Calculated account expected values
 			foreach( $tanks AS $tank ) {
 
 				// Tank exists in expected tank values
 				if( isset($this->expected_tank_values[$tank->tank_id]) ) {
 					
+					// Expected values for current tank
 					$expected = $this->expected_tank_values[$tank->tank_id];
-					$expDAMAGE += $expected->expDamage * $tank->statistics->battles;
-					$expSPOT += $expected->expSpot * $tank->statistics->battles;
-					$expFRAGS += $expected->expFrag * $tank->statistics->battles;
-					$expDEF += $expected->expDef * $tank->statistics->battles;
-					$expWIN += 0.01*$expected->expWinRate * $tank->statistics->battles;           
+					
+					// Battles on current tank
+					$tank_battles = $tank->statistics->battles;
+					
+					// Calculate expected values for current tank
+					$expDAMAGE += $expected->expDamage * $tank_battles;
+					$expSPOT += $expected->expSpot * $tank_battles;
+					$expFRAGS += $expected->expFrag * $tank_battles;
+					$expDEF += $expected->expDef * $tank_battles;
+					$expWIN += 0.01*$expected->expWinRate * $tank_battles;           
 					
 				// Tank missing in expected tank values so add it to the list
 				} else {
 					
 					$missing [] = $tank->tank_id;
 					
+				}
+			}
+			
+			// User want accurate calculation
+			if( $this->accurate_calculation AND !empty($missing) ) {
+				
+				// Get missing tanks stats from API server
+				$missing_tanks = $this->api->get('wot/tanks/stats',array('tank_id'=>implode(',',$missing),'fields'=>'tank_id,all.battles,all.frags,all.damage_dealt,all.dropped_capture_points,all.spotted,all.wins','account_id'=>$account_id))->$account_id;
+				
+				// Reduce account summary data
+				foreach( $missing_tanks AS $tank ) {
+					$summary->damage_dealt -= $tank->all->damage_dealt;
+					$summary->spotted -= $tank->all->spotted;
+					$summary->frags -= $tank->all->frags;
+					$summary->dropped_capture_points -= $tank->all->dropped_capture_points;
+					$summary->wins -= $tank->all->wins;
 				}
 			}
 
@@ -156,11 +201,11 @@ class WN8 {
 			}
 
 			// Calculate WN8
-			$rDAMAGE = $average->damage_dealt / $expDAMAGE;
-			$rSPOT = $average->spotted / $expSPOT;
-			$rFRAG = $average->frags / $expFRAGS;
-			$rDEF = $average->dropped_capture_points / $expDEF;
-			$rWIN = $average->wins / $expWIN;  
+			$rDAMAGE = $summary->damage_dealt / $expDAMAGE;
+			$rSPOT = $summary->spotted / $expSPOT;
+			$rFRAG = $summary->frags / $expFRAGS;
+			$rDEF = $summary->dropped_capture_points / $expDEF;
+			$rWIN = $summary->wins / $expWIN;  
 
 			$rWINc    = max(0,                      ($rWIN    - 0.71) / (1 - 0.71) );
 			$rDAMAGEc = max(0,                      ($rDAMAGE - 0.22) / (1 - 0.22) );
